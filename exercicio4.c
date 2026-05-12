6423233140b632128803668c8341fc0a03d1ef4a
Natali Vieira <nathynatali3@gmail.com>
2026-05-12 19:57:31 -0300

    exercicio4.c

--- exercicio4.c
+++ exercicio4.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-int main()
-{
-    int v[10];
-    int ;
 
-    for (i = 0; i < 10; i++)
-    {
-        printf("Informe v[%d]: ", i);
-        scanf("%d", &v[i]);
+int main() {
+    int vetor[10];
+    int maior, i;
+
+    for(i = 0; i < 10; i++) {
+        scanf("%d", &vetor[i]);
     }
-    printf("Todos os valores pares:\n");
-    for (i = 0; i < 10; i++)
-    {
-        acc = acc + v[i]; // acc = acc + v[i] é o mesmo que acc += v[i]
+
+    for(i = 1; i < 10; i++) {
+        if(vetor[i] > maior); {
+            maior = vetor[i]}
     }
-    printf("A soma de todos os valores é: %d\n", acc);
-    printf("\nFim do programa.\n");
+
+    printf("O maior valor do vetor é: %d\n", );
+
+    return 0;
 }
