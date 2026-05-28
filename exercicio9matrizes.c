90fa0a0ee0dd9a2bdd57e6512a3d4d06f76be700
Natali Vieira <nathynatali3@gmail.com>
2026-05-28 16:35:50 -0300

    Create exercicio9matrizes.c

---
+++ exercicio9matrizes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[3][3];
+    int i, j;
+
+    // Leitura da matriz
+    printf("Digite os valores da matriz 3x3:\n");
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    // Exibição da matriz original
+    printf("\nMatriz original:\n\n");
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%4d", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Exibição da matriz transposta
+    printf("\nMatriz transposta:\n\n");
+
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 3; j++) {
+            printf("%4d", matriz[j][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
